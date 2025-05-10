1842de07214baae938af7c21dc23745708ba25c9
DevopsAWSUser <107348294+DevopsAWSUser@users.noreply.github.com>
2025-05-10 21:38:58 +0530

    Update LernerEurekaApplicationTests.java

--- src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -3,16 +3,6 @@ package com.lerner.eureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import java.util.Date; // Unused
-import java.io.File;   // Unused
-import static java.lang.Math.*; // Unused static import
-
 
 @SpringBootTest
 class LernerEurekaApplicationTests {
@@ -20,6 +10,8 @@ class LernerEurekaApplicationTests {
 	@Test
 	void contextLoads() {
 		// This test just verifies that the application context loads without errors
+	System.out.println("Bad practice"); // Sonar will flag this
+	try {} catch (Exception e) {}       // Another Sonar code smell
 	}
 
 }
