070bb3d3573d4d97069f51c383425438443aaa1a
DevopsAWSUser <107348294+DevopsAWSUser@users.noreply.github.com>
2025-05-10 21:23:44 +0530

    Update LernerEurekaApplicationTests.java

--- src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -3,6 +3,11 @@ package com.lerner.eureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootTest
 class LernerEurekaApplicationTests {
 
