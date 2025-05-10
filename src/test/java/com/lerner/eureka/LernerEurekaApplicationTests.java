f2abfa7f3e462b4917f80d2677bf8b9c4660e7e9
DevopsAWSUser <107348294+DevopsAWSUser@users.noreply.github.com>
2025-05-10 21:32:09 +0530

    Update LernerEurekaApplicationTests.java

--- src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -3,28 +3,15 @@ package com.lerner.eureka;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// Unused imports
-import java.util.Date;
-import java.io.File;
-
-// Duplicate import
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-// Wrong package format (invalid separator '-')
-import org-slf4j.Logger; // already incorrect
-import org-springframework.beans.factory.annotation.Autowired;
-
-// Unsorted imports (Sonar likes them ordered)
-import org.springframework.boot.SpringApplication;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-// Unused static import
-import static java.lang.Math.*;
+import java.util.Date; // Unused
+import java.io.File;   // Unused
+import static java.lang.Math.*; // Unused static import
 
 
 @SpringBootTest
