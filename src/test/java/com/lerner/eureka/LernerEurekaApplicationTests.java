1b46f36ba02540e8ff2ed99f06739f7b85264cd3
DevopsAWSUser <107348294+DevopsAWSUser@users.noreply.github.com>
2025-05-10 22:07:25 +0530

    Update LernerEurekaApplicationTests.java

--- src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
+++ src/test/java/com/lerner/eureka/LernerEurekaApplicationTests.java
@@ -1,25 +1,14 @@
 package com.lerner.eureka;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootApplication
-@EnableEurekaServer
-public class LernerEurekaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LernerEurekaApplication.class, args);
-    }
+@SpringBootTest
+class LernerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test just verifies that the application context loads without errors
+	}
+
 }
